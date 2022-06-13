4fe4909afa38f9041cc472c3d710d17f7ac9be7a
VerS7 <103847309+VerS7@users.noreply.github.com>
2022-06-13 03:15:07 +0300

    Add files via upload

--- BotUtils.py
+++ BotUtils.py
@@ -3,13 +3,15 @@ import os
 import glob
 import requests
 import re
+import xlrd
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw, ImageFont
 
 
 class Schedule(object):
+    """Размер выходных изображений"""
+    size = (1200, 1500)
     """Функции с расписанием"""
-
     @staticmethod
     def rasparse(url):
         """Вычленение текста расписания с сайта по url"""
@@ -92,7 +94,8 @@ class Schedule(object):
             img.text((490, 160), group, font=otherfont, fill=(86, 131, 172))
             img.text((120, 265), " ".join(final), font=mainfont, fill=(86, 131, 172))
             img.text((290, 1300), update, font=otherfont, fill=(86, 131, 172))
-            back.save('rasp_pic.png')
+            image = back.resize(cls.size)
+            image.save('rasp_pic.png')
         except Exception as e:
             print(e)
 
@@ -111,7 +114,7 @@ class Schedule(object):
             for elem in rasp:
                 if len(elem) == 3:
                     rasp.pop(rasp.index(elem))
-            return rasp
+            return rasp[0:7]
         except Exception as e:
             print(e)
 
@@ -145,7 +148,97 @@ class Schedule(object):
                 img.text((410, 160), group, font=otherfont, fill=(86, 131, 172))
                 img.text((120, 265), " ".join(elem[1:]), font=mainfont, fill=(86, 131, 172))
                 img.text((290, 1300), update.strip(), font=otherfont, fill=(86, 131, 172))
-                back.save(f'{urltype}/{rasp.index(elem)}.png')
+                image = back.resize(cls.size)
+                image.save(f'{urltype}/{rasp.index(elem)}_{groupname}.png')
+        except Exception as e:
+            print(e)
+
+
+class ExcelSchedule(object):
+    """Высота расписаний в excel-файле"""
+    group_count = 10
+    """Функции excel-расписания"""
+    @staticmethod
+    def split(a, n):
+        """Деление списков"""
+        k, m = divmod(len(a), n)
+        return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+
+    @classmethod
+    def flatten(cls, L):
+        """Развёртка вложенных списков"""
+        for item in L:
+            if any(isinstance(i, list) for i in item):
+                yield from cls.flatten(item)
+            else:
+                yield item
+
+    @classmethod
+    def excel_parse(cls, filename, count=group_count):
+        """Парсинг эксель-файла на блоки с расписанием каждой группы"""
+        try:
+            file = xlrd.open_workbook(filename)
+            sheet = file.sheet_by_index(0)
+            vals = [sheet.row_values(rownum) for rownum in range(6, sheet.nrows)]
+            time = ''.join(sheet.row_values(1)).strip('на ')
+            rasps = []
+            for column in range(0, 4):
+                rasp = []
+                for elem in vals:
+                    x = list(cls.split(elem[3::], 4))
+                    rasp.append(x[column])
+                splited = list(cls.split(rasp, count))
+                for i in splited:
+                    f = list(cls.split(i[1::], 6))
+                    h = list(cls.flatten(f))
+                    out = [''.join(i[0]) + '\n', ''.join(i[0]) + '\n']
+                    for c in range(0, len(h)):
+                        if c % 2 == 0:
+                            out.append(f'Пара:\n'), out.append(''.join(h[c][::2]) + '\n'), out.append(''.join(h[c][2::]) + '\n')
+                        else:
+                            out.append(''.join(h[c][::2]) + '\n'), out.append(''.join(h[c][2::]) + '\n')
+                    final = []
+                    for j in out:
+                        if j != '\n':
+                            final.append(j)
+                    final[0] = time
+                    rasps.append(final)
+            return rasps
+        except Exception as e:
+            print(e)
+
+    @staticmethod
+    def excel_reading(groupname, excel_rasp):
+        """Возвращает расписание группы по тэгу"""
+        for elem in excel_rasp:
+            if groupname in elem[1]:
+                return elem
+
+    @classmethod
+    def excel_reading_img(cls, groupname, background, filename):
+        """Расписание на день в картинке"""
+        try:
+            mainfont = ImageFont.truetype("impact.ttf", size=35)
+            otherfont = ImageFont.truetype("impact.ttf", size=50)
+            back = Image.open(background)
+            img = ImageDraw.Draw(back)
+            txt = cls.excel_reading(groupname, cls.excel_parse(filename))
+            time = txt[0]
+            group = txt[1]
+            rasp = txt[2:len(txt)]
+            final = []
+            for line in rasp:
+                if len(line.split(" ")) > 5:
+                    line = " ".join(line.split(" ")[0:5]) + "..." + "\n"
+                elif "Пара" in line:
+                    line = '-' * 90 + '\n' + line
+                final.append(line)
+            final[0] = " " + final[0]
+            img.text((350, 100), time, font=otherfont, fill=(86, 131, 172))
+            img.text((490, 160), group, font=otherfont, fill=(86, 131, 172))
+            img.text((120, 265), " ".join(final), font=mainfont, fill=(86, 131, 172))
+            image = back.resize(Schedule.size)
+            image.save('exc_rasp_pic.png')
         except Exception as e:
             print(e)
 
@@ -220,9 +313,8 @@ class GatherTags(object):
     @classmethod
     def grouplist_create(cls):
         """Создаёт список групп"""
-        all = cls.tagsprettify()
         final = []
-        for i in all:
+        for i in cls.tagsprettify():
             final.append(i)
         return final
 
--- VKbot.py
+++ VKbot.py
@@ -3,7 +3,7 @@ import vk_api
 import time
 import configparser
 from BotUtils import *
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
 
@@ -13,11 +13,13 @@ class RaspBot:
         """Инициализация необходимых параметров"""
         self.config = configparser.ConfigParser()
         self.config.read("settings.ini")
+        Schedule.size = int(self.config['image']['width']), int(self.config['image']['length'])
+        ExcelSchedule.group_count = int(self.config['utility']['gpcount'])
+
         """Подключение к сообществу по токену и ID"""
         self.token = self.config['bot']['token']
-        self.id = self.config['bot']['pubid']
         self.vk_session = vk_api.VkApi(token=self.token)
-        self.botlongpoll = VkBotLongPoll(self.vk_session, self.id)
+        self.longpoll = VkLongPoll(self.vk_session)
         self.vk = self.vk_session.get_api()
         self.upload = vk_api.VkUpload(self.vk)
 
@@ -37,84 +39,103 @@ class RaspBot:
     def main(self):
         """Цикл прочитки сообщений ботом"""
         try:
-            for event in self.botlongpoll.listen():
-                if event.type == VkBotEventType.MESSAGE_NEW:
-                    message = event.obj['message']['text']
-                    peer_id = event.obj['message']['peer_id']
+            for event in self.longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    if not event.from_me:
+                        message = event.text
+                        peer_id = event.peer_id
 
-                    if self.comlist[0] in message.lower():
-                        """Запрос информации о боте"""
-                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message=self.info)
+                        if self.comlist[0] in message.lower():
+                            """Запрос информации о боте"""
+                            self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message=self.info)
 
-                    elif self.comlist[1] in message.lower():
-                        """Запрос доступных групп"""
-                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="\n".join(self.grouplist))
+                        elif self.comlist[1] in message.lower():
+                            """Запрос доступных групп"""
+                            self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="\n".join(self.grouplist))
 
-                    elif self.comlist[4] in message.lower():
-                        """Запрос расписания картинкой"""
-                        try:
-                            if len(message.split()) == 2:
-                                if GatherTags.groupname_validation(message.split()[1], self.grouplist) is True:
+                        elif self.comlist[4] in message.lower():
+                            """Запрос расписания картинкой"""
+                            try:
+                                if len(message.split()) == 2:
+                                    if GatherTags.groupname_validation(message.split()[1], self.grouplist) is True:
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Подождите, идёт обработка.")
+                                        Schedule.reading_img(message.split()[1], "raspback.png")
+                                        photo = self.upload.photo_messages('rasp_pic.png')
+                                        owner_id = photo[0]['owner_id']
+                                        photo_id = photo[0]['id']
+                                        access_key = photo[0]['access_key']
+                                        attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), attachment=attachment, message='Расписание для группы {0}'.format(message.split()[1]))
+                                    else:
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Произошла ошибка.")
+                                else:
+                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Отсутствует номер группы.")
+                            except Exception as e:
+                                print(e)
+
+                        elif self.comlist[2] in message.lower():
+                            """Запрос недельного расписания в картинках"""
+                            try:
+                                if len(message.split()) == 2:
+                                    Util.img_clear('cg/*')
+                                    if GatherTags.groupname_validation(message.split()[1], self.grouplist) is True:
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Подождите, идёт обработка.")
+                                        Schedule.weekreading_img(message.split()[1], "raspback.png", tags=self.tags)
+                                        attachment = []
+                                        for image in glob.glob('cg/*.png'):
+                                            photo = self.upload.photo_messages(image)
+                                            owner_id = photo[0]['owner_id']
+                                            photo_id = photo[0]['id']
+                                            access_key = photo[0]['access_key']
+                                            attachment.append(f'photo{owner_id}_{photo_id}_{access_key}')
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), attachment=attachment, message='Недельное расписание для группы {0}'.format(message.split()[1]))
+                                    else:
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message='Произошла ошибка.')
+                                else:
+                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Отсутствует номер группы.")
+                            except Exception as e:
+                                print(e)
+
+                        elif self.comlist[3] in message.lower():
+                            """Запрос основного расписания в картинках"""
+                            try:
+                                if len(message.split()) == 2:
+                                    Util.img_clear('bg/*')
+                                    if GatherTags.groupname_validation(message.split()[1], self.grouplist) is True:
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Подождите, идёт обработка.")
+                                        Schedule.weekreading_img(message.split()[1], "raspback.png", tags=self.tags, urltype='bg')
+                                        attachment = []
+                                        for image in glob.glob('bg/*.png'):
+                                            photo = self.upload.photo_messages(image)
+                                            owner_id = photo[0]['owner_id']
+                                            photo_id = photo[0]['id']
+                                            access_key = photo[0]['access_key']
+                                            attachment.append(f'photo{owner_id}_{photo_id}_{access_key}')
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), attachment=attachment, message='Основное расписание для группы {0}'.format(message.split()[1]))
+                                    else:
+                                        self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message='Произошла ошибка.')
+                                else:
+                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Отсутствует номер группы.")
+                            except Exception as e:
+                                print(e)
+
+                        elif self.comlist[5] in message.lower():
+                            """Запрос заочного расписания картинкой"""
+                            try:
+                                if len(message.split()) == 2:
                                     self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Подождите, идёт обработка.")
-                                    Schedule.reading_img(message.split()[1], "raspback.png")
-                                    photo = self.upload.photo_messages('rasp_pic.png')
+                                    file = glob.glob('excel_files/*')
+                                    ExcelSchedule.excel_reading_img(message.split()[1], "raspback.png", filename=file[len(file)-1])
+                                    photo = self.upload.photo_messages('exc_rasp_pic.png')
                                     owner_id = photo[0]['owner_id']
                                     photo_id = photo[0]['id']
                                     access_key = photo[0]['access_key']
                                     attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), attachment=attachment, message='Расписание для группы {0}'.format(message.split()[1]))
+                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), attachment=attachment, message='Заочное расписание для группы {0}'.format(message.split()[1]))
                                 else:
                                     self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Произошла ошибка.")
-                            else:
-                                self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Отсутствует номер группы.")
-                        except Exception as e:
-                            print(e)
-
-                    elif self.comlist[2] in message.lower():
-                        """Запрос недельного расписания в картинках"""
-                        try:
-                            if len(message.split()) == 2:
-                                Util.img_clear('cg/*')
-                                if GatherTags.groupname_validation(message.split()[1], self.grouplist) is True:
-                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Подождите, идёт обработка.")
-                                    Schedule.weekreading_img(message.split()[1], "raspback.png", tags=self.tags)
-                                    attachment = []
-                                    for image in glob.glob('cg/*.png'):
-                                        photo = self.upload.photo_messages(image)
-                                        owner_id = photo[0]['owner_id']
-                                        photo_id = photo[0]['id']
-                                        access_key = photo[0]['access_key']
-                                        attachment.append(f'photo{owner_id}_{photo_id}_{access_key}')
-                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), attachment=attachment, message='Недельное расписание для группы {0}'.format(message.split()[1]))
-                                else:
-                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message='Произошла ошибка.')
-                            else:
-                                self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Отсутствует номер группы.")
-                        except Exception as e:
-                            print(e)
-
-                    elif self.comlist[3] in message.lower():
-                        """Запрос основного расписания в картинках"""
-                        try:
-                            if len(message.split()) == 2:
-                                Util.img_clear('bg/*')
-                                if GatherTags.groupname_validation(message.split()[1], self.grouplist) is True:
-                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Подождите, идёт обработка.")
-                                    Schedule.weekreading_img(message.split()[1], "raspback.png", tags=self.tags, urltype='bg')
-                                    attachment = []
-                                    for image in glob.glob('bg/*.png'):
-                                        photo = self.upload.photo_messages(image)
-                                        owner_id = photo[0]['owner_id']
-                                        photo_id = photo[0]['id']
-                                        access_key = photo[0]['access_key']
-                                        attachment.append(f'photo{owner_id}_{photo_id}_{access_key}')
-                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), attachment=attachment, message='Основное расписание для группы {0}'.format(message.split()[1]))
-                                else:
-                                    self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message='Произошла ошибка.')
-                            else:
-                                self.vk.messages.send(peer_id=peer_id, random_id=get_random_id(), message="Отсутствует номер группы.")
-                        except Exception as e:
-                            print(e)
+                            except Exception as e:
+                                print(e)
 
         except Exception as e:
             print(e)
