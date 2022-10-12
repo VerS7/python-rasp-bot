872ab276a4d1bf656ba2b6ce3aaa2d690d3ea15b
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2022-10-12 20:57:35 +0300

    fixed bug with urls

--- rasp_api/__init__.py
+++ rasp_api/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .Schedule import Schedule
-from .ExcelSchedule import ExcelSchedule
+#from .ExcelSchedule import ExcelSchedule
 from .GatherTags import GatherTags
 from .Util import Util
 from .vkUtils import BotHandler
