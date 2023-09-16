5328fd9ec8b2f5b54b14637749e7376c57035589
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-09-16 06:28:21 +0300

    week/main schedule & fixes

--- rasp_api/NewSchedule.py
+++ rasp_api/NewSchedule.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from .Parsing import *
+from typing import Union, List
 from PIL import Image, ImageDraw, ImageFont
 
 
 IMAGE_SIZE = (1200, 1500)
 MAIN_IMAGE_FONT = ImageFont.truetype("impact.ttf", size=35)
 OTHER_IMAGE_FONT = ImageFont.truetype("impact.ttf", size=50)
-BG_IMAGE = "./raspback.png"
+BG_IMAGE = "./files/raspback.png"
 
 
-def prettify_for_image(schedule: dict) -> list:
+def __prettify_for_image(schedule: list) -> list:
     """
     Возвращает подготовленный для изображения список строк
     :param list schedule: список расписания конкретной группы
@@ -18,92 +19,108 @@ def prettify_for_image(schedule: dict) -> list:
     for elem in schedule:
         result.append(" ".join(elem[0:2]))
         if len(elem) > 2:
-            result.append("\n".join(elem[2::]))
+            print(elem)
+            result.append("\n".join([__prettify_length(line) for line in elem[2::]]))
         else:
             pass
         result.append("-" * 90)
     return result
 
 
-def daily_image(groupname: str):
-    """Расписание на день в картинке"""
+def __prettify_length(string: str) -> str:
+    """
+    Укорачивает длинные строки
+    :param string: Строка
+    :return: Изначальная или укороченная строка
+    """
+    if len(string) > 55:
+        if len(" ".join(string.split()[0:5])) > 55:
+            return " ".join(string.split()[0:4]) + "..."
+        else:
+            return " ".join(string.split()[0:5]) + "..."
+    return string
+
+
+def daily_image(groupname: str, resize_multiplier: Union[None, float] = None) -> Image.Image:
+    """
+    Расписание на день в PIL Image object
+    :param str groupname: название/номер группы
+    :param resize_multiplier: Множитель размера изображения. 1 = default, 0.5 - изображение в 2 раза меньше
+    """
     background = Image.open(BG_IMAGE)
     rasp_image = ImageDraw.Draw(background)
 
-    parsed = parse_request(URL_DAILY)
-    update = get_update(parsed)
-    time = get_day(parsed)
-    daily = prettify_for_image(get_daily_by_groupname(groupname, soup=parsed))
+    update = get_update()
+    time = get_day()
+    daily = __prettify_for_image(get_daily(groupname))
 
     rasp_image.text((460, 100), time, font=OTHER_IMAGE_FONT, fill=(86, 131, 172))
     rasp_image.text((510, 160), groupname, font=OTHER_IMAGE_FONT, fill=(86, 131, 172))
     rasp_image.text((120, 240), "\n".join(daily), font=MAIN_IMAGE_FONT, fill=(86, 131, 172))
     rasp_image.text((290, 1300), update, font=OTHER_IMAGE_FONT, fill=(86, 131, 172))
-    background.save("gaysex.png")
-    # return background.resize(IMAGE_SIZE)
-#
-# def weekreading(groupname: str, tags: dict, urltype='cg'):
-#     """Недельное расписание ДИНО"""
-#     url = 'http://dmitrov-dubna.ru/shedule/{0}{1}.htm'.format(urltype, GatherTags.tagsearch(groupname, tags))
-#     api = requests.get(url)
-#     content = api.text
-#     soup = BeautifulSoup(content, 'html.parser')
-#     group = soup.find('h1').get_text().encode('latin1').decode('cp1251')
-#     update = soup.find('div', {'class': 'ref'}).get_text().encode('latin1').decode('cp1251')
-#     soup = soup.findAll(['td', 'a', 'div'], {'class': {'hd', 'hd0', 'z1', 'z2', 'z3'}})
-#     out = []
-#     for tag in soup:
-#         out.append(tag.get_text(strip=True, separator='|').encode('latin1').decode('cp1251'))
-#     del out[0:8]
-#     out = '|'.join(out).replace(':|', ': ').split('|')
-#     return out, group, update
-#
-#
-# def weekreading_chuck(lst):
-#     """Разделение недельного расписания по дням"""
-#     rasp = []
-#     out = []
-#     for x in lst:
-#         if len(x) > 0:
-#             out.append(x)
-#         else:
-#             rasp.append(out.copy())
-#             out.clear()
-#     for elem in rasp:
-#         if len(elem) == 3:
-#             rasp.pop(rasp.index(elem))
-#     return rasp[0:7]
-#
-#
-# def weekreading_img(groupname, background, tags, urltype='cg'):
-#     """Недельное/основное расписание картинками"""
-#     mainfont = ImageFont.truetype("impact.ttf", size=35)
-#     otherfont = ImageFont.truetype("impact.ttf", size=50)
-#     txt, group, update = Schedule.weekreading(groupname, tags, urltype=urltype)
-#     lst = Schedule.weekreading_chuck(txt)
-#     final = []
-#     rasp = []
-#     pilobjs = []
-#     for elem in lst:
-#         for line in elem:
-#             if len(line.split(" ")) > 5:
-#                 line = " ".join(line.split(" ")[0:5]) + "..." + "\n"
-#             elif line == '6':
-#                 line = '-' * 90 + '\n' + line + " Пара:\n"
-#             elif "Пара" in line:
-#                 line = '-' * 90 + '\n' + line + '\n'
-#             else:
-#                 line = line + '\n'
-#             final.append(line)
-#         rasp.append(final.copy())
-#         final.clear()
-#     for elem in rasp:
-#         back = Image.open(background)
-#         img = ImageDraw.Draw(back)
-#         img.text((460, 100), elem[0], font=otherfont, fill=(86, 131, 172))
-#         img.text((410, 160), group, font=otherfont, fill=(86, 131, 172))
-#         img.text((120, 265), " ".join(elem[1:]), font=mainfont, fill=(86, 131, 172))
-#         img.text((290, 1300), update.strip(), font=otherfont, fill=(86, 131, 172))
-#         image = back.resize(Schedule.size)
-#         pilobjs.append(image)
-#     return pilobjs
+
+    if resize_multiplier:
+        return background.resize((int(IMAGE_SIZE[0] * resize_multiplier), int(IMAGE_SIZE[1] * resize_multiplier)))
+    else:
+        return background.resize(IMAGE_SIZE)
+
+
+def __create_images(grouptag: str, rasp_type: str, resize_multiplier: Union[None, float] = None) -> List[Image.Image]:
+    """
+    Создаёт список недельного/основого расписания в PIL Image objects
+    :param str grouptag: тэг группы
+    :param str rasp_type: тип расписания. week - недельное, main - основное
+    :param resize_multiplier: Множитель размера изображения. 1 = default, 0.5 - изображение в 2 раза меньше
+    :return: Список с PIL объектами изображений недельного расписания
+    """
+    rasp = []
+    result = []
+    size = IMAGE_SIZE
+
+    if rasp_type == "week":
+        rasp = get_group_week(grouptag)
+    if rasp_type == "main":
+        rasp = get_group_main(grouptag)
+
+    if resize_multiplier:
+        size = (int(IMAGE_SIZE[0] * resize_multiplier), int(IMAGE_SIZE[1] * resize_multiplier))
+
+    group: str = rasp[0]
+    update: str = rasp[1]
+    schedule: List[dict] = rasp[2]
+
+    for day in schedule:
+        background = Image.open(BG_IMAGE)
+        rasp_image = ImageDraw.Draw(background)
+        prettified = __prettify_for_image(list(*day.values()))
+
+        rasp_image.text((550, 100), str(*list(day.keys())), font=OTHER_IMAGE_FONT, fill=(86, 131, 172))
+        rasp_image.text((510, 160), group, font=OTHER_IMAGE_FONT, fill=(86, 131, 172))
+        rasp_image.text((120, 240), "\n".join(prettified), font=MAIN_IMAGE_FONT, fill=(86, 131, 172))
+        rasp_image.text((290, 1300), update, font=OTHER_IMAGE_FONT, fill=(86, 131, 172))
+
+        result.append(background.resize(size))
+
+    return result
+
+
+def weekly_images(grouptag: str, resize_multiplier: Union[None, float] = None) -> List[Image.Image]:
+    """
+    Создаёт список недельного расписания в PIL Image objects
+    :param str grouptag: тэг группы
+    :param resize_multiplier: Множитель размера изображения. 1 = default, 0.5 - изображение в 2 раза меньше
+    :return: Список с PIL объектами изображений недельного расписания
+    """
+    return __create_images(grouptag, "week", resize_multiplier)
+
+
+def mainly_images(grouptag: str, resize_multiplier: Union[None, float] = None) -> List[Image.Image]:
+    """
+    Создаёт список основого расписания в PIL Image objects
+    :param str grouptag: тэг группы
+    :param resize_multiplier: Множитель размера изображения. 1 = default, 0.5 - изображение в 2 раза меньше
+    :return: Список с PIL объектами изображений недельного расписания
+    """
+    return __create_images(grouptag, "week", resize_multiplier)
+
+
