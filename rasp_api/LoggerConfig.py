360756fb25f42c371e44777b71bc49738a915319
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-10-03 01:36:48 +0300

    log formatting

--- rasp_api/LoggerConfig.py
+++ rasp_api/LoggerConfig.py
@@ -1,15 +1,54 @@
 import logging
 import sys
-import os
 
+from os import path
+from datetime import datetime
+
+
+class Formatter(logging.Formatter):
+    max_level_length = 9
+    max_module_length = 14
+    max_func_length = 15
+    max_line_length = 5
+    max_thread_length = 14
+
+    def format(self, record: logging.LogRecord) -> str:
+        level, module, func, line, thread = \
+            record.levelname, record.module, record.funcName, record.lineno, record.threadName
+
+        message = record.msg.__str__().replace('\n', ' ')
+
+        time = datetime.now().strftime('%Y-%d-%m / %H:%M:%S')
+
+        if len(level) <= self.max_level_length:
+            level = f"{level}" + " " * (self.max_level_length - len(level))
+
+        if len(module) <= self.max_module_length:
+            module = f"{module}" + " " * (self.max_module_length - len(module))
+
+        if len(func) + len(record.args) <= self.max_func_length:
+            func = f"{func}{record.args}" + " " * (self.max_func_length - len(func))
+
+        if len(str(line)) <= self.max_line_length:
+            line = f"{line}" + " " * (self.max_line_length - len(str(line)))
+
+        if len(thread) <= self.max_thread_length:
+            thread = f"{thread}" + " " * (self.max_thread_length - len(thread))
+
+        return f"{time} [ {level} ] | {thread} | {module} | Func: {func} | Line: {line} | {message}"
+
+
+formatter = Formatter()
 
 consoleHandler = logging.StreamHandler(stream=sys.stdout)
-filelogHandler = logging.FileHandler(filename=f"{os.path.dirname(os.path.abspath(__file__))}/debug.log")
+consoleHandler.setFormatter(formatter)
 
+filelogHandler = logging.FileHandler(filename=f"{path.join(path.dirname(path.abspath(__file__)), '../files/debug.log')}")
+filelogHandler.setFormatter(formatter)
+
+basic_format = "%(asctime)s | [%(levelname)s] | %(module)s Func: %(funcName)s() Line: %(lineno)d| %(message)s"
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s | [%(levelname)s] | %(module)s Func: %(funcName)s() Line: %(lineno)d| %(message)s",
-    datefmt='%Y-%d-%m / %H:%M:%S',
     handlers=[consoleHandler, filelogHandler]
 )
