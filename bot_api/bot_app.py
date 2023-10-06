bafef5379de2f5a364fe7576ed2bf61f3264c63a
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-10-07 02:13:55 +0300

    docstring fix

--- bot_api/bot_app.py
+++ bot_api/bot_app.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+"""
+Основная логика приложения.
+"""
+from os import getenv, path
+
 from rasp_api.Schedule import daily_image, weekly_images
 from rasp_api.GatherTags import get_tags
 from rasp_api.Parsing import URL_WEEKLY
 from rasp_api.groupValidator import get_tag, validate_groupname
 
 from bot_api.asyncBot import AsyncVkBot
-from bot_api.chatsConnector import Chats
-from bot_api.Utility import *
-
-from os import getenv, path
+from bot_api.chats_connector import Chats
+from bot_api.notificator import Notificator
+from bot_api.Utility import image_to_bytes
 
 try:
     from dotenv import load_dotenv
@@ -20,13 +24,19 @@ except ModuleNotFoundError:
 token = getenv("VK_TOKEN")
 pub_id = int(getenv("PUBLIC_ID"))
 
-BotApp = AsyncVkBot(token, pub_id, admin_ids=[406579945])  # Бот
-ChatSystem = Chats()
+ChatSystem = Chats()  # Подключенные к оповещению чаты
+Notifier = Notificator(ChatSystem, timings=["01:45"])  # Система оповещений
+BotApp = AsyncVkBot(token, pub_id, admin_ids=[406579945], notificator=Notifier)  # Бот
 
 
 # Возвращает расписание на день по группе в диалог
 @BotApp.command(command="расп", replaceable=True, placeholder="Подождите, идёт обработка...")
 async def send_daily(peer, args):
+    """
+    Вовращает ежедневное расписание по номеру группы или подключенному к чату номеру.
+    :param peer: id чата
+    :param args: аргументы, переданные при вызове команды через чат
+    """
     if args is None:
         if ChatSystem.in_chats(peer):
             groupname = ChatSystem.get_group(peer)
@@ -46,6 +56,11 @@ async def send_daily(peer, args):
 # Возвращает недельное расписание по группе в диалог
 @BotApp.command(command="неделя", replaceable=True, placeholder="Подождите, идёт обработка...")
 async def send_weekly(peer, args):
+    """
+    Вовращает ежеднедельное расписание по номеру группы или подключенному к чату номеру.
+    :param peer: id чата
+    :param args: аргументы, переданные при вызове команды через чат
+    """
     if args is None:
         if ChatSystem.in_chats(peer):
             groupname = ChatSystem.get_group(peer)
@@ -65,6 +80,11 @@ async def send_weekly(peer, args):
 # Возвращает доступные группы в диалог
 @BotApp.command(command="группы")
 async def send_groups(peer, args):
+    """
+    Возвращает в чат все доступные к вызову номера групп
+    :param peer: id чата
+    :param args: аргументы, переданные при вызове команды через чат
+    """
     groups = '\n'.join(get_tags(URL_WEEKLY).keys())
     return peer, f"Доступные группы: \n{groups}."
 
@@ -72,6 +92,11 @@ async def send_groups(peer, args):
 # Подключает диалог к системе оповещений/быстрых вызовов
 @BotApp.command(command="подключить")
 async def notify_connect(peer, args):
+    """
+    Подключает конкретный чат к номеру группы.
+    :param peer: id чата
+    :param args: аргументы, переданные при вызове команды через чат
+    """
     if ChatSystem.in_chats(peer):
         return peer, f"Данный чат уже подключён с номером группы: {ChatSystem.get_group(peer)}"
 
@@ -83,23 +108,33 @@ async def notify_connect(peer, args):
         ChatSystem.add_group(peer, validated)
         return peer, f"К чату с ID {peer} подключена группа {validated}"
 
-    return peer, f"Неправильный номер группы."
+    return peer, "Неправильный номер группы."
 
 
 # Отключает диалог от системы оповещений/быстрых вызовов
 @BotApp.command(command="отключить")
 async def notify_disconnect(peer, args):
+    """
+    Отключает чат от номера группы. Вызывается без аргументов
+    :param peer: id чата
+    :param args: аргументы, переданные при вызове команды через чат
+    """
     group = ChatSystem.remove_chat(peer)
 
     if group is not None:
         return peer, f"Данный чат отключён от группы: {group}"
 
-    return peer, f"Данный чат не подключён к системе оповещений."
+    return peer, "Данный чат не подключён к системе оповещений."
 
 
 # Админ-функционал
 @BotApp.command(command="admin", admin=True)
 async def admin_manage(peer, args):
+    """
+    Debug админ система
+    :param peer: id чата
+    :param args: аргументы, переданные при вызове команды через чат
+    """
     if args is None:
         return peer, "admin <command>\n" \
                      "mypeer: peer_id чата\n" \
