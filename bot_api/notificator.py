d02cd33d97a5aa958e9aef85dd1b58051d2032c5
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-10-08 05:11:28 +0300

    fix empty schedule notifications

--- bot_api/notificator.py
+++ bot_api/notificator.py
@@ -2,10 +2,11 @@
 """
 Модуль системы оповещений.
 """
-from typing import Callable
+from typing import Callable, Union, Any
 from asyncio import sleep
 
-from rasp_api.schedule import daily_image
+from rasp_api.schedule import daily_image, is_empty
+from rasp_api.parsing import parse_request, get_daily, URL_DAILY
 from rasp_api.log_conf import *
 
 from bot_api.utility import image_to_bytes
@@ -26,7 +27,7 @@ class Notificator:
         self.__timings = timings
         self.__cd = cooldown_s
 
-    async def run(self, sender: Callable, image_loader: Callable = None):
+    async def run(self, sender: Callable, image_loader: Callable):
         """
         :param sender: Функция отправления оповещения
         :param image_loader: Функция загрузки изображения
@@ -35,19 +36,17 @@ class Notificator:
 
         while True:
             current_time = datetime.now().strftime("%H:%M")
-
             if current_time in self.__timings:
                 logging.info(f"Оповещение по времени: {current_time}.")
+                parsed = parse_request(URL_DAILY)
                 for chat, group in self.__chats.get_chats().items():
-                    if image_loader is not None:
-                        image = await image_loader(chat, image_to_bytes(daily_image(group)))
+                    if is_empty(get_daily(group, parsed)):
+                        image = await image_loader(chat,
+                                                   image_to_bytes(daily_image(group, parsed, 0.7)))
+                        logging.info(f"Оповещение для группы: {group}. PeerID: {chat}")
+                        await sender(chat,
+                                     f"{current_time} | повещение расписания для группы {group}",
+                                     image)
                     else:
-                        image = None
-
-                    logging.info(f"Оповещение для группы: {group}. PeerID: {chat}")
-
-                    await sender(chat,
-                                 f"{current_time} | повещение расписания для группы {group}",
-                                 image)
-
+                        logging.info(f"Расписание для группы {group} Пустое.")
             await sleep(self.__cd)
