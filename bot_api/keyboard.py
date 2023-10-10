d7cf5f20215d0563867ef397038a340523ecb869
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-10-10 04:03:46 +0300

    vk keyboard

---
+++ bot_api/keyboard.py
@@ -0,0 +1,39 @@
+"""
+Клавиатуры для ВК диалогов.
+"""
+import json
+
+
+BASIC_KEYBOARD = {
+    "inline": False,
+    "buttons": [
+        [{"action": {"type": "text",
+                     "label": "Дневное расписание",
+                     "payload": {"command": "расп"}},
+          "color": "secondary"},
+
+         {"action": {"type": "text",
+                     "label": "Недельное расписание",
+                     "payload": {"command": "нрасп"}},
+          "color": "secondary"}
+         ],
+        [{"action": {"type": "text",
+                     "label": "Группы",
+                     "payload": {"command": "группы"}},
+          "color": "secondary"},
+
+         {"action": {"type": "text",
+                     "label": "Помощь",
+                     "payload": {"command": "инфо"}},
+          "color": "secondary"}
+         ]
+    ]
+}
+
+
+def get_keyboard_string(keyboard: dict) -> str:
+    """
+    :param dict keyboard: VK Keyboard в виде dict.
+    :return: jsonified строка
+    """
+    return json.dumps(keyboard)
