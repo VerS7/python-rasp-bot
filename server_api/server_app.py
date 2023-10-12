792788797aa729d4acb4affefd3937d7ca56d544
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-10-12 04:57:17 +0300

    ask gpt function

--- server_api/server_app.py
+++ server_api/server_app.py
@@ -5,6 +5,7 @@
 from os import getenv
 
 import gevent.pywsgi
+import g4f
 
 from flask import request, jsonify
 from flask_cors import CORS
@@ -15,7 +16,6 @@ from g4f.gui.server import website, backend
 from server_api.schedule import html_daily
 from rasp_api.log_conf import *
 
-
 try:
     from dotenv import load_dotenv
 
@@ -23,23 +23,42 @@ try:
 except ModuleNotFoundError:
     pass
 
+g4f.logging = False
+
 CORS(app)
 host, port = getenv("host"), int(getenv("port"))
 
 
-@app.route('/v1/api', methods=['GET'])
-def get_daily():
+@app.route('/v1/api/schedule', methods=['POST'])
+def daily_schedule():
     """
     :return: jsonified html of daily schedule
     """
     data = request.json
-    if data["rasp_type"] == "daily":
-        return jsonify({'message': "".join(html_daily(data["groupname"]))})
+    return jsonify({'message': "".join(html_daily(data["groupname"]))})
+
+
+@app.route('/v1/api/gpt', methods=['POST'])
+def ask_gpt():
+    """
+    :return: jsonified one way chatgpt response
+    """
+    prompt = request.json["prompt"]
+
+    response = g4f.ChatCompletion.create(
+        model=g4f.models.gpt_35_turbo,
+        messages=[{"role": "user", "content": prompt}])
+
+    if len(response) > 2900:
+        response = "Ошибка. Попробуйте ещё раз."
+
+    return jsonify({"answer": response})
 
 
 def run_app(local: bool = False):
     """
     запускает Flask-приложение для API и Gpt4Free UI приложения.
+    :param bool local: localhost/host
     """
     if local:
         host_ = "localhost"
