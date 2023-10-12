8d2805fc6e732f95b2b341df89c3bdf5e07aef12
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-10-12 04:58:39 +0300

    web working thread

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import threading
 
 from bot_api.bot_app import *
+from server_api.server_app import run_app
 
 
 async_event_loop = asyncio.new_event_loop()
@@ -18,16 +19,12 @@ def run_bot(event_loop):
     BotApp.run()
 
 
-def run_api_server(event_loop):
-    """
-    :param event_loop: asyncio event loop
-    """
-    raise NotImplementedError
-
-
 main_thread = threading.Thread(target=run_bot, args=(async_event_loop,))
+web_thread = threading.Thread(target=run_app)
 
 
 if __name__ == "__main__":
     main_thread.start()
+    web_thread.start()
     main_thread.join()
+    web_thread.join()
