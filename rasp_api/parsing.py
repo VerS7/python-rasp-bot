92c9978dd401149ffae3b01b920b95317ee2b047
Albert <thegoversus@gmail.com>
2024-08-25 08:06:44 +0300

    new async parsing

--- rasp_api/parsing.py
+++ rasp_api/parsing.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*-
 """
 Парсинг данных с сайта расписания.
 """
 import re
-from typing import Tuple, List
+from typing import Tuple, List, Dict
+from asyncio import new_event_loop, get_event_loop, AbstractEventLoop
 
-import requests
+from aiohttp import ClientSession
+from aiohttp.web import HTTPException
+
 from bs4 import BeautifulSoup
-
+from bs4.element import Tag
 
 URL_DAILY = "http://dmitrov-dubna.ru/shedule/hg.htm"
 URL_WEEKLY = "http://dmitrov-dubna.ru/shedule/cg.htm"
@@ -15,160 +17,304 @@ URL_MAINLY = "http://dmitrov-dubna.ru/shedule/bg.htm"
 URL_RESULTS = "http://dmitrov-dubna.ru/shedule/vg.htm"
 URL_TEACHER = "http://dmitrov-dubna.ru/shedule/vp.htm"
 
+PARSER = "html.parser"
 
-def parse_request(url: str) -> "BeautifulSoup":
+
+class Parser:
     """
-    Парсит страницу по ссылке
-    :param str url: url-ссылка на страницу
+    Базовый парсер
     """
-    try:
-        api = requests.get(url)
-    except Exception as e:
-        raise e
-    if api.status_code != 200:
-        raise requests.RequestException("Сайт не отвечает.")
-    return BeautifulSoup(api.text, 'html.parser')
-
-
-def get_all_daily(soup: BeautifulSoup) -> dict:
-    """Возвращает текущее дневное расписание для всех групп."""
-    groups = {}
-    current = []
-    for elem in soup.select('.inf'):
-        if len(elem) < 100:
-            raise Exception("Ошибка в получении данных с сайта.")
-        for row in list(elem)[4:]:
-            if row.get_text() == '\n':
-                if current:
-                    if '\n' in current:
-                        current.remove('\n')
-                    group_name = current[0][0]
-                    del current[0][0]
-                    groups[group_name] = current
-                current = [row]
-            elif len(row.get_text()) > 1:
-                current.append(row.get_text(strip=True, separator="|")
-                                  .encode('latin1')
-                                  .decode('cp1251')
-                                  .split("|"))
-    return groups
-
-
-def get_group_week(grouptag: str) -> Tuple[str, str, List[dict]]:
-    """
-    Возвращает недельное расписание для конкретной группы по groupname
-    :param str grouptag: тэг конкретной группы
-    :return: группа, обновление, недельное расписание
-    """
-    return __get_week_or_main(parse_request(URL_WEEKLY.replace("cg", f"cg{grouptag}")))
-
-
-def get_group_main(grouptag: str) -> Tuple[str, str, List[dict]]:
-    """
-    Возвращает основное расписание для конкретной группы по groupname
-    :param str grouptag: номер/название группы
-    :return: группа, обновление, основное расписание
-    """
-    return __get_week_or_main(parse_request(URL_WEEKLY.replace("bg", f"bg{grouptag}")))
-
-
-def __get_week_or_main(soup: BeautifulSoup) -> Tuple[str, str, List[dict]]:
-    week = []
-    temp = []
-    group = soup.find("div").find('h1').get_text().encode('latin1').decode('cp1251').split()[1]
-    for elem in soup.select('.inf'):
-        for row in list(elem)[5:]:
-            if "\n" in row:
-                if "День" not in temp[0]:  # Проверка на День Пара Неделя 2. Костыль :/
-                    if len(temp[0][0]) > 9:  # Проверка на полную дату. Костыль :/
-                        day = f"{temp[0][0]}  {temp[0][1]}"
-                        del temp[0][0]  # Удаление полной даты
-                        del temp[0][0]  # Удаление дня недели
-                    else:
-                        day = temp[0][0]
-                        del temp[0][0]
-                    week.append({day: temp.copy()})
-                temp.clear()
+    def __init__(self, loop: AbstractEventLoop | None, session: ClientSession | None = None):
+        if not loop:
+            if get_event_loop() is None:
+                self.loop = new_event_loop()
             else:
-                text = row.get_text(strip=True, separator="|")\
-                          .encode('latin1')\
-                          .decode('cp1251')\
-                          .split("|")
+                self.loop = get_event_loop()
+        else:
+            self.loop = loop
 
-                if not len(text) == 1:
-                    temp.append(text)
+        if not session:
+            self.session = ClientSession(loop=self.loop)
+        else:
+            self.session = session
 
-    return group, get_update(), week
+    async def parse_request(self, url: str) -> BeautifulSoup:
+        """
+        Парсит страницу по ссылке
+        :param str url: url страницы
+        """
+        async with self.session.get(url) as response:
+            if response.status != 200:
+                raise HTTPException(text="Сайт не отвечает.")
+
+            return BeautifulSoup(await response.read(), features=PARSER)
+
+    def get_update(self, soup: BeautifulSoup) -> str:
+        """
+        Возвращает строку состояния последнего обновления расписания
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        return soup.find('.ref').get_text()
+
+    def get_day(self, soup: BeautifulSoup) -> str:
+        """
+        Возвращает строку текущего дня
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        return soup.find('.zgr').get_text()
+
+    def get_exact_update(self, soup: BeautifulSoup) -> Tuple[str, str]:
+        """
+        Возвращает конкретное значение даты и времени обновления
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        match = re.search(r"(\d{2}\.\d{2}\.\d{4})\sв\s(\d{2}:\d{2})", self.get_update(soup))
+        if match:
+            return match.group(1), match.group(2)
+
+    def _process_raw_schedule(self, raw: List[Tag]) -> Tuple[str, List[Tuple[str, Tuple[str | None] | None]]]:
+        schedule_header = raw[0].get_text(separator=" ")
+        headers = []
+        contents = []
+
+        for elem in raw[1::]:
+            if elem.attrs["class"][0] == "hd":
+                headers.append(elem.get_text(separator=" ").replace("\n", ""))
+
+            if elem.attrs["class"][0] == "ur":
+                z1, z2, z3 = (elem.find("a", class_="z1"),
+                              elem.find("a", class_="z2"),
+                              elem.find("a", class_="z3"))
+
+                contents.append((z1.get_text() if z1 else None,
+                                 z2.get_text() if z2 else None,
+                                 z3.get_text() if z3 else None))
+
+            if elem.attrs["class"][0] == "nul":
+                contents.append(None)
+
+        return schedule_header, list(zip(headers, contents))
 
 
-def get_daily(groupname: str, soup: BeautifulSoup = None) -> list:
+class TagsParser(Parser):
     """
-    Возвращает list расписания на день по имени группы или None
-    :param str groupname: номер/название группы
+    Парсинг тэгов и названий групп
     """
-    if soup:
-        parsed = soup
-    else:
-        parsed = parse_request(URL_DAILY)
+    def __init__(self, loop: AbstractEventLoop | None = None, session: ClientSession | None = None):
+        super().__init__(loop, session)
+        self._soup = None
+        self._tags: Dict[str, str] = {}
 
-    return get_all_daily(parsed).get(groupname)
+    async def parse_tags(self, soup: BeautifulSoup | None = None, force=False) -> Dict[str, str]:
+        """
+        Парсит название групп и тэгов
+        :param BeautifulSoup soup: распарсеный HTML
+        :param bool force: получить тэги по новой если они уже существуют
+        """
+        if soup:
+            self._soup = soup
 
+        if force or not self._tags:
+            self._soup = await self.parse_request(URL_WEEKLY)
 
-def get_update(soup: BeautifulSoup = None) -> str:
-    """Возвращает строку состояния последнего обновления расписания"""
-    if soup:
-        parsed = soup
-    else:
-        parsed = parse_request(URL_DAILY)
+            for a in self._soup.find("table", {"class": "inf"}).find_all("a", {"class": "z0"}):
+                self._tags[a.get_text()] = re.search(r"(?<=\D)\d+", a.attrs["href"]).group()
 
-    data = parsed.select('.ref')
-    update = data[0].get_text().encode('latin1').decode('cp1251').strip("\r\n")
-    return update
+            return self._tags
 
+        if self._tags:
+            return self._tags
 
-def get_day(soup: BeautifulSoup = None) -> str:
-    """Возвращает строку текущего дня"""
-    if soup:
-        parsed = soup
-    else:
-        parsed = parse_request(URL_DAILY)
+    async def validate(self, groupname: str) -> Tuple[str, str] | None:
+        """
+        Возвращает Tuple с названием группы и тэгом по первому совпадению groupname
+        :param str groupname:
+        """
+        if not self._tags:
+            await self.parse_tags()
 
-    soup = parsed.select('.zgr')
-    day = soup[0].get_text().encode('latin1').decode('cp1251').strip("\r\n")
-    if len(day) > 10:
-        return day
-    raise Exception("Не удалось получить текущий день.")
-
-
-def get_exact_update(soup: BeautifulSoup = None) -> tuple:
-    """Возвращает конкретное значение даты и времени обновления"""
-    if soup:
-        parsed = soup
-    else:
-        parsed = parse_request(URL_DAILY)
-
-    match = re.search(r"(\d{2}\.\d{2}\.\d{4})\sв\s(\d{2}:\d{2})", get_update(parsed))
-    if match:
-        return match.group(1), match.group(2)
-    raise Exception("Не удалось получить дату и время.")
+        for k, v in self._tags.items():
+            if groupname in k:
+                return k, v
 
 
-def is_empty(day_schedule: list) -> bool:
+class DailyParser(Parser):
     """
-    True/false пустое расписание или нет. (Пустое == нет пар!)
-    :param list day_schedule: Расписание на день.
+    Парсинг ежедневного расписания по всем группам
     """
-    if not(isinstance(day_schedule[0], list)):
-        raise Exception("Некорректное расписание.")
-    return len(day_schedule) > 6
+    def __init__(self, loop: AbstractEventLoop | None = None, session: ClientSession | None = None):
+        super().__init__(loop, session)
+        self._soup = None
+
+    async def get_all_daily(self) -> Dict[str, List[Tuple[str, Tuple[str | None] | None]]]:
+        """Возвращает текущее дневное расписание для всех групп."""
+        if not self._soup:
+            self._soup = await self.parse_request(URL_DAILY)
+
+        groups = {}
+        current = []
+
+        for td in self._soup.find("table", {"class": "inf"}).find_all("td")[4::]:
+            if td.attrs["class"][0] == "hd0":  # Разделитель групп
+                group, schedule = self._process_raw_schedule(current)
+                groups[group] = schedule
+                current = []
+                continue
+
+            current.append(td)
+
+        return groups
+
+    async def get_daily(self, groupname: str) -> List[Tuple[str, Tuple[str | None] | None]]:
+        """
+        Возвращает список расписания на день по имени группы или None
+        :param str groupname: номер/название группы
+        """
+        all_daily = await self.get_all_daily()
+        return all_daily.get(groupname)
+
+    async def get_update(self, soup: BeautifulSoup | None = None) -> str:
+        """
+        Возвращает строку состояния последнего обновления расписания
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        if soup:
+            self._soup = soup
+
+        if not self._soup:
+            self._soup = await self.parse_request(URL_DAILY)
+
+        return super().get_update(self._soup)
+
+    async def get_day(self, soup: BeautifulSoup | None = None) -> str:
+        """
+        Возвращает строку текущего дня
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        if soup:
+            self._soup = soup
+
+        if not self._soup:
+            self._soup = await self.parse_request(URL_DAILY)
+
+        return super().get_day(self._soup)
+
+    async def get_exact_update(self, soup: BeautifulSoup | None = None) -> Tuple[str, str]:
+        """
+        Возвращает конкретное значение даты и времени обновления
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        if soup:
+            self._soup = soup
+
+        if not self._soup:
+            self._soup = await self.parse_request(URL_DAILY)
+
+        return super().get_exact_update(self._soup)
 
 
-def get_all_teachers(soup: BeautifulSoup) -> list:
-    raise NotImplementedError
-
-
-def get_daily_teacher(teacher: str, soup: BeautifulSoup) -> list:
+class WeekParser(Parser):
     """
-    :param str teacher: Фамилия/Имя препода.
+    Парсинг недельного расписания конкретной группы
     """
-    raise NotImplementedError
+    def __init__(self, grouptag: str, loop: AbstractEventLoop | None = None, session: ClientSession | None = None):
+        super().__init__(loop, session)
+        self._url = URL_WEEKLY.replace("cg", f"cg{grouptag}")
+        self._soup = None
+
+    async def get_update(self, soup: BeautifulSoup | None = None) -> str:
+        """
+        Возвращает строку состояния последнего обновления расписания
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        if soup:
+            self._soup = soup
+
+        if not self._soup:
+            self._soup = await self.parse_request(self._url)
+
+        return super().get_update(self._soup)
+
+    async def get_exact_update(self, soup: BeautifulSoup | None = None) -> Tuple[str, str]:
+        """
+        Возвращает конкретное значение даты и времени обновления
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        if soup:
+            self._soup = soup
+
+        if not self._soup:
+            self._soup = await self.parse_request(self._url)
+
+        return super().get_exact_update(self._soup)
+
+    async def get_week(self,
+                       soup: BeautifulSoup | None = None
+                       ) -> Dict[str, List[Tuple[str, Tuple[str | None] | None]]]:
+        """
+        Возвращает недельное расписание.
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        if soup:
+            self._soup = soup
+
+        if not self._soup:
+            self._soup = await self.parse_request(self._url)
+
+        days = {}
+        current = []
+
+        for td in self._soup.find("table", {"class": "inf"}).find_all("td")[8::]:
+            if td.attrs["class"][0] == "hd0":  # Разделитель групп
+                if len(current) == 0:  # Пропуск в случае если не набралось расписание
+                    continue
+                day, schedule = self._process_raw_schedule(current)
+                days[day] = schedule
+                current = []
+                continue
+
+            if td.attrs["class"][0] == "hd" and td.get_text() in ("День", "Неделя 1", "Неделя 2", "Пара"):
+                # Пропуск td, в котором находится структурная информация
+                continue
+
+            current.append(td)
+
+        return days
+
+
+class MainParser(WeekParser):
+    """
+    Парсинг основного расписания конкретной группы
+    """
+    def __init__(self, grouptag: str, loop: AbstractEventLoop | None = None, session: ClientSession | None = None):
+        super().__init__(grouptag, loop, session)
+        self._url = URL_MAINLY.replace("bg", f"bg{grouptag}")
+
+    async def get_main(self,
+                       soup: BeautifulSoup | None = None
+                       ) -> Dict[str, List[Tuple[str, Tuple[str | None] | None]]]:
+        """
+        Возвращает основное расписание.
+        :param BeautifulSoup soup: распарсеный HTML
+        """
+        if soup:
+            self._soup = soup
+
+        if not self._soup:
+            self._soup = await self.parse_request(self._url)
+
+        return await self.get_week(self._soup)
+
+
+class TeachersParser(Parser):
+    def __init__(self, loop: AbstractEventLoop | None, session: ClientSession | None = None):
+        super().__init__(loop, session)
+
+    def get_all_teachers(self, soup: BeautifulSoup) -> list:
+        raise NotImplementedError
+
+    def get_daily_teacher(self, teacher: str, soup: BeautifulSoup) -> list:
+        """
+        :param str teacher: Фамилия/Имя препода
+        """
+        raise NotImplementedError
